890b580bc788c83e2c6634c4f9c441e026ee5c91
Leonardo <leonardo.paganelli@iteris.com.br>
2019-05-26 12:18:02 -0300

    ajust interface name

--- src/services/encryption/providers/CodeNationDataProvider.ts
+++ src/services/encryption/providers/CodeNationDataProvider.ts
@@ -1,6 +1,6 @@
 import request from "request-promise";
 
-export const getChallenge = async (profileToken: string): Promise<IDecryptMessage> => {
+export const getChallenge = async (profileToken: string): Promise<IChallengeMessage> => {
     const url = `https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${profileToken}`;
     const response = await request(url);
     return JSON.parse(response);
